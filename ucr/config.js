2748e34c0742a055984e31050662711c6501a939
Phil Archer <phila@w3.org>
2017-05-25 15:43:12 +0100

    UCR skeleton in place

---
+++ ucr/config.js
@@ -0,0 +1,39 @@
+var respecConfig = {
+//	preProcess: [dfn_index],
+    specStatus: "ED",
+    shortName: "dxwg-ucr",
+//    previousMaturity:     "NOTE",
+//    previousPublishDate:  "2016-07-23",
+//    previousURI:          "https://www.w3.org/TR/2016/NOTE-poe-ucr-20160721/",
+    edDraftURI: "https://w3c.github.io/dxwg/ucr/",
+   editors: [{
+  name:       "Ixchel Faniel",
+  company:    "OCLC (Online Computer Library Center, Inc.)",
+  companyURL: "https://www.oclc.org/"
+},
+{
+name: "Jaroslav Pullmann",
+company: "Fraunhofer Gesellschaft",
+companyURL: "https://www.fraunhofer.de/"
+},
+{
+name: "Rob Atkinson",
+company: "Metalinkage, Open Geospatial Consortium",
+companyURL: "http://www.opengeospatial.org/"
+}
+         	],
+    wg: "Dataset Exchange Working Group",
+    wgURI: "https://www.w3.org/2017/dxwg/",
+    wgPublicList: "public-dxwg-comments",
+    wgPatentURI: "https://www.w3.org/2004/01/pp-impl/99375/status",
+    inlineCSS: "true",
+	lint: "false",
+    noRecTrack: "true",
+    localBiblio: {
+      "SCHEMA-ORG":{
+        "href":"http://schema.org/",
+        "title":"Schema.org"
+       },
+    },
+    issueBase: "https://github.com/w3c/dxwg/issues"
+};
