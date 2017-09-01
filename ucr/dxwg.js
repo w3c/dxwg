32cbcae0882e035b3179121f5367e7c35d97e2fe
Jaroslav Pullmann <jaroslav.pullmann@fit.fraunhofer.de>
2017-09-01 11:03:32 +0200

    Added function to automatically link related requirements to use cases, removed explicit references from UCs

--- ucr/dxwg.js
+++ ucr/dxwg.js
@@ -17,3 +17,36 @@ function toggleVisibility(elem) {	// expand or collapse the next div
 		}
 	}
 }
+
+// Extension to create back-references from requirements to related use cases
+function addRelatedRequirements(){
+	var reqs = document.getElementsByClassName("requirement");
+	for (var i = 0; i < reqs.length; i++){
+		var req = reqs[i]; 
+		var links = req.querySelectorAll("p.relatedUseCases > a");
+		for (var j = 0; j < links.length; j++){
+			var link = links[j];
+			var id = link.getAttribute("href").replace("#","");
+			var uc = document.getElementById(id);
+			addRelatedRequirement(uc,req);
+		} 
+	}
+}
+
+// Augments supplied use case by a link to the related requirement
+function addRelatedRequirement(uc,r){
+	// Related requirements are enclosed within the last p element
+	var relatedReqs = uc.querySelectorAll("p.relatedRequirements")[0];
+	var reqLink = createSectionLink(r);
+		relatedReqs.appendChild(reqLink);		
+}
+
+// Creates a local link to supplied section (usecase or requirement)
+function createSectionLink(section){
+	// label added by respec script, otherwise {section.h3}
+	var a  = document.createElement("a");
+	a.setAttribute("href", "#" + section.id);	
+	return a;
+}
+
+addRelatedRequirements();
