b919d219c2fa6cdfdf502ee0fc65acd5d88ba24c
larsgsvensson <lars.g.svensson@gmail.com>
2018-05-17 18:51:40 +0200

    Created new directory for conneg deliverable and moved files from "profiles"

---
+++ conneg-by-ap/config.js
@@ -0,0 +1,30 @@
+var respecConfig = {
+    specStatus: "ED",
+    shortName: "dxwg-profiles",
+    edDraftURI: "https://w3c.github.io/dxwg/profiles/",
+    editors: [{
+        name:       "Ruben Verborgh",
+        url:        "https://ruben.verborgh.org/",
+        company:    "Ghent University – imec",
+        companyURL: "http://idlab.ugent.be/",
+      },
+      {
+        name: "Lars G. Svensson",
+        company: "Deutsche Nationalbibliothek",
+        companyURL: "http://www.dnb.de/",
+      },
+      {
+        name: "Rob Atkinson",
+        company: "Metalinkage, Open Geospatial Consortium",
+        companyURL: "http://www.opengeospatial.org/",
+      },
+    ],
+    wg: "Dataset Exchange Working Group",
+    wgURI: "https://www.w3.org/2017/dxwg/",
+    wgPublicList: "public-dxwg-comments",
+    wgPatentURI: "https://www.w3.org/2004/01/pp-impl/99375/status",
+    inlineCSS: "true",
+    lint: "false",
+    noRecTrack: "true",
+    issueBase: "https://github.com/w3c/dxwg/issues",
+};
