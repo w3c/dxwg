038e68e8ccfedd5c70272653ca7510817d0e2c87
kcoyle <kcoyle@kcoyle.net>
2018-05-22 07:51:14 +0200

    Named editors

--- profiles/config.js
+++ profiles/config.js
@@ -2,22 +2,22 @@ var respecConfig = {
     specStatus: "ED",
     shortName: "dxwg-profiles",
     edDraftURI: "https://w3c.github.io/dxwg/profiles/",
-    editors: [{
-        name:       "Ruben Verborgh",
-        url:        "https://ruben.verborgh.org/",
-        company:    "Ghent University – imec",
-        companyURL: "http://idlab.ugent.be/",
-      },
-      {
-        name: "Lars G. Svensson",
-        company: "Deutsche Nationalbibliothek",
-        companyURL: "http://www.dnb.de/",
-      },
+    editors: [
       {
         name: "Rob Atkinson",
         company: "Metalinkage, Open Geospatial Consortium",
         companyURL: "http://www.opengeospatial.org/",
       },
+	  {
+		  name: "Karen Coyle",
+		  company: "Dublin Core Metadata Initiative",
+		  companyURL: "http://dublincore.org/",
+	  },
+	  {
+		  name: "Antoine Isaac",
+		  company: "Europeana",
+		  companyURL: "https://www.europeana.eu/",
+	  },
     ],
     wg: "Dataset Exchange Working Group",
     wgURI: "https://www.w3.org/2017/dxwg/",
