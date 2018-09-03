fdb56449b89cbcba11faa68660319af951b819d2
Nicholas Car <nicholas.car@csiro.au>
2018-09-03 11:33:23 +1000

    previous work examples with refs & defn table

--- profiles/config.js
+++ profiles/config.js
@@ -6,18 +6,18 @@ var respecConfig = {
       {
         name: "Rob Atkinson",
         company: "Metalinkage, Open Geospatial Consortium",
-        companyURL: "http://www.opengeospatial.org/",
+        companyURL: "http://www.opengeospatial.org/"
       },
 	  {
 		  name: "Karen Coyle",
 		  company: "Dublin Core Metadata Initiative",
-		  companyURL: "http://dublincore.org/",
+		  companyURL: "http://dublincore.org/"
 	  },
 	  {
 		  name: "Antoine Isaac",
 		  company: "Europeana",
-		  companyURL: "https://www.europeana.eu/",
-	  },
+		  companyURL: "https://www.europeana.eu/"
+	  }
     ],
     authors: [{
         name:       "Nicholas Car",
@@ -33,4 +33,36 @@ var respecConfig = {
     lint: "false",
     noRecTrack: "true",
     issueBase: "https://github.com/w3c/dxwg/issues",
+    localBiblio: {
+        "DCAP": {
+            "authors": [
+                "Karen Coyle",
+                "Thomas Baker"
+            ],
+            "href": "http://dublincore.org/documents/dcmi-terms/",
+            "title": "Guidelines for Dublin Core Application Profiles (Working Draft)",
+            "date": " 2009-05-18",
+            "status": "DCMI Recommended Resource"
+        },
+        "DCSF": {
+            "authors": [
+                "Mikael Nilsson",
+                "Thomas Baker",
+                "Pete Johnston"
+            ],
+            "href": "http://dublincore.org/documents/singapore-framework/",
+            "title": "The Singapore Framework for Dublin Core Application Profiles",
+            "date": " 2008-01-14",
+            "status": "DCMI Recommended Resource"
+        },
+        "DCDSP": {
+            "authors": [
+                "Nilsson, Mikael"
+            ],
+            "href": "http://dublincore.org/documents/dc-dsp/",
+            "title": "Description Set Profiles: A constraint language for Dublin Core Application Profiles",
+            "date": " 2008-03-31",
+            "status": "DCMI Working Draft"
+        }
+    }
 };
