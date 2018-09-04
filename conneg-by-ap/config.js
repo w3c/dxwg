ca5520822ca366e3f39e828d793f9d73b9cbb281
Nicholas Car <nicholas.car@csiro.au>
2018-09-04 17:55:14 +1000

    moved stub text to ReSpec Issues

--- conneg-by-ap/config.js
+++ conneg-by-ap/config.js
@@ -35,17 +35,27 @@ var respecConfig = {
     noRecTrack: "true",
     issueBase: "https://github.com/w3c/dxwg/issues",
     localBiblio: {
+        "PROF-CONNEG": {
+            "href": "https://www.w3.org/TR/conneg-by-ap/",
+            "title": "Content Negotiation by Profile",
+            "date": " 2018-12-31",
+            "status": "W3C Editor's Draft"
+        },
         "PROF-GUIDE": {
             "href": "https://www.w3.org/TR/profile-guidance/",
             "title": "Profile Guidance",
             "date": " 2018-12-31",
             "status": "W3C Editor's Draft"
         },
-        "PROF-CONNEG": {
-            "href": "https://www.w3.org/TR/conneg-by-ap/",
-            "title": "Content Negotiation by Profile",
-            "date": " 2018-12-31",
-            "status": "W3C Editor's Draft"
+        "PROF-IETF": {
+            "authors": [
+                "L. Svensson",
+                "R. Verborgh"
+            ],
+            "href": "https://profilenegotiation.github.io/I-D-Accept--Schema/I-D-accept-schema/",
+            "title": "Negotiating Profiles in HTTP",
+            "date": " 2017-10-24",
+            "status": "IETF Internet Draft"
         }
     }
 };
