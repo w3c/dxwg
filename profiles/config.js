4a9b40441b8a5d0de1991377b9af8b8eef3d2adb
Nicholas Car <nicholas.car@csiro.au>
2018-12-16 05:54:19 +1000

    refs to PROF docs updated to DX-... localBiblio updated to FPWDs for Ont & Conneg Nick moved from Author to Editor

--- profiles/config.js
+++ profiles/config.js
@@ -3,28 +3,28 @@ var respecConfig = {
     shortName: "profile-guidance",
     edDraftURI: "https://w3c.github.io/dxwg/profiles/",
     editors: [
-      {
-        name: "Rob Atkinson",
-        company: "Metalinkage, Open Geospatial Consortium",
-        companyURL: "http://www.opengeospatial.org/"
-      },
-	  {
-		  name: "Karen Coyle",
-		  company: "Dublin Core Metadata Initiative",
-		  companyURL: "http://dublincore.org/"
-	  },
-	  {
-		  name: "Antoine Isaac",
-		  company: "VU Amsterdam and Europeana",
-		  companyURL: "https://www.europeana.eu/"
-	  }
+        {
+            name: "Rob Atkinson",
+            company: "Metalinkage, Open Geospatial Consortium",
+            companyURL: "http://www.opengeospatial.org/"
+        },
+        {
+            name: "Karen Coyle",
+            company: "Dublin Core Metadata Initiative",
+            companyURL: "http://dublincore.org/"
+        },
+        {
+            name: "Antoine Isaac",
+            company: "VU Amsterdam and Europeana",
+            companyURL: "https://www.europeana.eu/"
+        },
+        {
+            name:       "Nicholas J. Car",
+            url:        "https://people.csiro.au/Nicholas-Car",
+            company:    "CSIRO",
+            companyURL: "https://www.csiro.au/"
+        }
     ],
-    authors: [{
-        name:       "Nicholas Car",
-        url:        "https://people.csiro.au/Nicholas-Car",
-        company:    "CSIRO",
-        companyURL: "https://www.csiro.au/"
-    }],
     wg: "Dataset Exchange Working Group",
     wgURI: "https://www.w3.org/2017/dxwg/",
     wgPublicList: "public-dxwg-comments",
@@ -36,66 +36,77 @@ var respecConfig = {
     issueBase: "https://github.com/w3c/dxwg/issues/",
     localBiblio: {
         "DCAP": {
-            "authors": [
+            authors: [
                 "Karen Coyle",
                 "Thomas Baker"
             ],
-            "href": "http://dublincore.org/documents/dcmi-terms/",
-            "title": "Guidelines for Dublin Core Application Profiles (Working Draft)",
-            "date": " 2009-05-18",
-            "status": "DCMI Recommended Resource"
+            href: "http://dublincore.org/documents/dcmi-terms/",
+            title: "Guidelines for Dublin Core Application Profiles (Working Draft)",
+            date: " 2009-05-18",
+            status: "DCMI Recommended Resource"
         },
         "DCSF": {
-            "authors": [
+            authors: [
                 "Mikael Nilsson",
                 "Thomas Baker",
                 "Pete Johnston"
             ],
-            "href": "http://dublincore.org/documents/singapore-framework/",
-            "title": "The Singapore Framework for Dublin Core Application Profiles",
-            "date": " 2008-01-14",
-            "status": "DCMI Recommended Resource"
+            href: "http://dublincore.org/documents/singapore-framework/",
+            title: "The Singapore Framework for Dublin Core Application Profiles",
+            date: " 2008-01-14",
+            status: "DCMI Recommended Resource"
         },
         "DCDSP": {
-            "authors": [
+            authors: [
                 "Nilsson, Mikael"
             ],
-            "href": "http://dublincore.org/documents/dc-dsp/",
-            "title": "Description Set Profiles: A constraint language for Dublin Core Application Profiles",
-            "date": " 2008-03-31",
-            "status": "DCMI Working Draft"
+            href: "http://dublincore.org/documents/dc-dsp/",
+            title: "Description Set Profiles: A constraint language for Dublin Core Application Profiles",
+            date: " 2008-03-31",
+            status: "DCMI Working Draft"
         },
-        "PROF-CONNEG": {
-            "href": "https://www.w3.org/TR/conneg-by-ap/",
-            "title": "Content Negotiation by Profile",
-            "date": " 2018-12-31",
-            "status": "W3C Editor's Draft"
+        "DX-PROF-CONNEG": {
+            editors: [
+                "Lars G. Svensson",
+                "Rob Atkinson",
+                "Nicholas J. Car"
+            ],
+            href: "https://www.w3.org/TR/2018/WD-dx-prof-conneg-20181218/",
+            title: "Content Negotiation by Profile",
+            date: " 2018-12-18",
+            status: "W3C First Public Working Draft"
         },
-        "PROF-GUIDE": {
-            "href": "https://www.w3.org/TR/profile-guidance/",
-            "title": "Profile Guidance",
-            "date": " 2018-12-31",
-            "status": "W3C Editor's Draft"
+        "DX-PROF-GUIDANCE": {
+            editors: [
+                "Rob Atkinson",
+                "Karen Coyle",
+                "Antoine Isaac",
+                "Nicholas J. Car"
+            ],
+            href: "https://www.w3.org/TR/profile-guidance/",
+            title: "Profile Guidance",
+            date: " 2018-12-31",
+            status: "W3C Editor's Draft"
         },
-        "PROF-IETF": {
-            "authors": [
+        "DX-PROF-IETF": {
+            authors: [
                 "L. Svensson",
                 "R. Verborgh"
             ],
-            "href": "https://profilenegotiation.github.io/I-D-Accept--Schema/I-D-accept-schema/",
-            "title": "Negotiating Profiles in HTTP",
-            "date": " 2017-10-24",
-            "status": "IETF Internet Draft"
+            href: "https://profilenegotiation.github.io/I-D-Accept--Schema/I-D-accept-schema/",
+            title: "Negotiating Profiles in HTTP",
+            date: " 2017-10-24",
+            status: "IETF Internet Draft"
         },
-        "PROF-ONT": {
+        "DX-PROF": {
             editors: [
                 "Rob Atkinson",
                 "Nicholas J. Car"
             ],
-            href: "https://www.w3.org/TR/vocab-prof/",
+            href: "https://www.w3.org/TR/2018/WD-dx-prof-20181218/",
             title: "Profile Descriptions ontology",
-            date: "2018-09-15",
-            status: "W3C Working Group Note"
+            date: "2018-12-18",
+            status: "W3C First Public Working Draft"
         },
         "vocab-dcat-2": {
             editors: [
