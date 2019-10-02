f8dea04fde90fb12b185f72467148b8109ac2a40
David Browning <33936348+davebrowning@users.noreply.github.com>
2019-10-02 14:13:57 +0200

    Test 3

--- dcat/config.js
+++ dcat/config.js
@@ -11,7 +11,7 @@ var respecConfig = {
     prevRecURI:           "https://www.w3.org/TR/REC-vocab-dcat-20140116/",    
 //    previousPublishDate:  "2014-01-16",
 //    previousMaturity:     "REC",
-//    previousURI:          "https://www.w3.org/TR/2014/REC-vocab-dcat-20140116/",
+    previousURI:          "https://www.w3.org/TR/2019/WD-vocab-dcat-2-20190528/",
     edDraftURI:           "https://w3c.github.io/dxwg/dcat/",
 //    issueBase:            "https://github.com/w3c/dxwg/issues/", -- Not needed when github used
     alternateFormats: [{
