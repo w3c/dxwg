029c352770a386a92679b22149e0ba8996bbd5c2
Andrea Perego <andrea-perego@users.noreply.github.com>
2019-10-17 00:22:09 +0200

    Initial commit

---
+++ dcat-implementation-report/config.js
@@ -0,0 +1,87 @@
+var respecConfig = {
+  specStatus: "base",
+  editors: [{
+    name:       "Riccardo Albertoni",
+    orcid:  "0000-0001-5648-2713",
+    company:    "CNR - Consiglio Nazionale delle Ricerche, Italy",
+    url: "http://www.imati.cnr.it/joomla/index.php/people/8-curricula/178-riccardo-albertoni",
+    companyURL: "http://www.cnr.it/",
+    w3cid: 72458
+  },{
+    name: "David Browning",
+    company: "Refinitiv",
+    companyURL: "https://www.refinitiv.com",
+    w3cid: 98704
+  },{
+    name:       "Simon Cox",
+    orcid:      "0000-0002-3884-3420",
+    company:    "CSIRO",
+    url:        "http://people.csiro.au/Simon-Cox",
+    companyURL: "https://www.csiro.au/",
+    w3cid: 1796
+  },{
+    name:       "Alejandra Gonzalez Beltran",
+    orcid:      "0000-0003-3499-8262",
+    company:    "Oxford eResearch Centre, Engineering Science, University of Oxford",
+    url: "https://www.oerc.ox.ac.uk/people/alejandra",
+    companyURL: "http://www.oerc.ox.ac.uk/",
+    w3cid: 61587
+  },{
+    name:       "Andrea Perego",
+    orcid:      "0000-0001-9300-2694",
+    company:    "European Commission, Joint Research Centre",
+    companyURL: "https://ec.europa.eu/jrc/",
+    w3cid: 40317
+  },{
+    name: "Peter Winstanley",
+    company: "Scottish Government",
+    companyURL: "http://www.gov.scot/",
+    w3cid: 64374
+  }],
+  otherLinks: [{
+    key: "Contributors",
+    data: [{
+      value: "Lars G. Svensson",
+      href:  "https://orcid.org/0000-0002-8714-9718"
+    }]
+  }],
+  processVersion: 2019,
+  edDraftURI: "https://w3c.github.io/dxwg/dcat-implementation-report/",
+  shortName: "dcat-implementation-report",
+  localBiblio: {
+    "CiteDCAT-AP": {
+      "href": "https://ec-jrc.github.io/datacite-to-dcat-ap/",
+      "title": "DataCite to DCAT-AP Mapping",
+      "authors": ["Andrea Perego","Timothy Austin"],
+      "status": "Working Draft",
+      "publisher":"European Commission, Joint Research Centre (JRC)",
+      "date": "2019"
+    },
+    "DCAT-AP-EXT":{
+      "href":"https://joinup.ec.europa.eu/solution/dcat-application-profile-data-portals-europe/document/national-extensions-analysis-dcat-ap",
+      "title":"Analysis of the DCAT-AP extensions",
+      "publisher":"European Commission",
+      "date":"2017"
+    },
+    "DCAT-AP-IG":{
+      "href":"https://joinup.ec.europa.eu/solution/dcat-application-profile-implementation-guidelines",
+      "title":"DCAT application profile implementation guidelines",
+      "publisher":"European Commission",
+      "date":"2016"
+    },
+    "DCAT-AP-JRC": {
+      "href": "https://ec-jrc.github.io/dcat-ap-jrc/",
+      "title": "DCAT-AP-JRC: An extension to DCAT-AP for multidisciplinary research data",
+      "authors": ["Andrea Perego"],
+      "status": "Working Draft",
+      "publisher":"European Commission, Joint Research Centre (JRC)",
+      "date": "2019"
+    },
+    "DCAT-AP-USE":{
+      "href":"https://joinup.ec.europa.eu/document/report-dcat-ap-use",
+      "title":"Report on DCAT-AP Use",
+      "publisher":"European Commission",
+      "date":"2017"
+    }
+  }
+};
