f4afd87f49322ed2b9eebe9f994fe511faeb7b9c
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2020-01-17 18:56:29 +0000

    errata

--- dcat/config.js
+++ dcat/config.js
@@ -1,7 +1,9 @@
 var respecConfig = {
 //	preProcess: [dfn_index],
 //    subtitle: "Version 2",
-    specStatus: "ED",
+    specStatus: "REC",
+    errata: "https://w3c.github.io/dxwg/dcat/errata",
+//   specStatus: "ED",
 //    specStatus: "CR",
 //  crEnd: "2019-10-24",
     implementationReportURI: "https://w3c.github.io/dxwg/dcat-implementation-report/",
